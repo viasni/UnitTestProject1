747c23f4d3fad2937491d26b98ff75ca314f27be
vijay kumar <kvijaykumar.testing@gmail.com>
2021-09-01 16:24:12 +0530

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestProject1.Configuration;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("Browser"));
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("Username"));
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("Password"));
+            Console.WriteLine((int)BrowserType.Firefox);
+            Console.WriteLine((int)BrowserType.Chrome);
+        }
+    }
+}
